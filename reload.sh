7ffc62411522ade0e868a0acb72c84720206f5bb
albert <fengelom@qq.com>
2018-12-02 11:03:30 +0800

    add reload

---
+++ reload.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# update code
+git checkout .
+git pull origin master
+
+# copy config
+rm -rf conf/app.conf
+cp conf/app.conf.bat conf/app.conf
+
+# stop fs
+supervisorctl stop api
+
+# get listen port
+pid = `lsof -i:9999 | grep LISTEN | awk -F '[ ]+' '{print $2}'`
+
+# kill port listen
+kill ${pid}
+
+# start fs
+supervisorctl stop api
+
+# re grant authorization
+chmod -R 755 restart.sh
